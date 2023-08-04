16844b8d68c75b832fea47281ae6b56d69fdcf15
izzahfatini <izzahfatini@graduate.utm.my>
2023-08-04 11:41:05 +0800

    first commit

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsComponent } from './products.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { ViewAllProductsComponent } from './view-all-products/view-all-products.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+
+
+@NgModule({
+  declarations: [
+    ProductsComponent,
+    CreateProductComponent,
+    ViewProductComponent,
+    ViewAllProductsComponent,
+    UpdateProductComponent,
+    DeleteProductComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule
+  ]
+})
+export class ProductsModule { }
